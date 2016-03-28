5f2573dbbfb7356ef41f42b2013531b3298de24d
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2016-03-28 20:26:16 +0700

    Initial commit

---
+++ lib/Class/Accessor/Array.pm
@@ -0,0 +1,74 @@
+package Class::Accessor::Array;
+
+# DATE
+# VERSION
+
+sub import {
+    my ($class0, $spec) = @_;
+    my $caller = caller();
+
+    no warnings 'redefine';
+
+    # generate accessors
+    for my $meth (keys %{$spec->{accessors}}) {
+        my $idx = $spec->{accessors}{$meth};
+        my $code_str = 'sub (;$) { ';
+        $code_str .= "\$_[0][$idx] = \$_[1] if \@_ > 1; ";
+        $code_str .= "\$_[0][$idx]; ";
+        $code_str .= "}";
+        #say "D:accessor code for $meth: ", $code_str;
+        *{"$caller\::$meth"} = eval $code_str;
+        die if $@;
+    }
+
+    # generate constructor
+    {
+        my $code_str = 'sub { my $class = shift; bless [], $class }';
+
+        #say "D:constructor code for class $caller: ", $code_str;
+        unless (*{"$caller\::new"}{CODE}) {
+            *{"$caller\::new"} = eval $code_str;
+            die if $@;
+        };
+    };
+}
+
+1;
+# ABSTRACT: Generate accessors/constructor for array-based object
+
+=for Pod::Coverage .+
+
+=head1 SYNOPSIS
+
+In F<lib/Your/Class.pm>:
+
+ package Your::Class;
+ use Class::Accessor::Array {
+     accessors => {
+         foo => 0,
+         bar => 1,
+     },
+ };
+
+In code that uses your class:
+
+ use Your::Class;
+
+ my $obj = Your::Class->new;
+ $obj->foo(1980);
+ $obj->bar(12);
+
+C<$obj> is now:
+
+ bless([1980, 12], "Your::Class");
+
+
+=head1 DESCRIPTION
+
+This module is a builder for array-backed classes.
+
+
+=head1 SEE ALSO
+
+Other class builders for array-backed objects: L<Class::XSAccessor::Array>,
+L<Class::ArrayObjects>, L<Object::ArrayType::New>.
---
+++ t/01-basic.t
@@ -0,0 +1,21 @@
+#!perl
+
+use 5.010001;
+use strict;
+use warnings;
+
+use FindBin '$Bin';
+use lib "$Bin/lib";
+
+use Test::More 0.98;
+
+use Local::C1;
+
+my $c1 = Local::C1->new;
+#is_deeply($c1, bless([undef, undef], "Local::C1"));
+
+$c1->foo(1980);
+$c1->bar(12);
+is_deeply($c1, bless([1980, 12], "Local::C1"));
+
+done_testing;
