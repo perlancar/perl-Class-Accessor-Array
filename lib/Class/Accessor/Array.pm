bcfe27b0ee756f9c2df111768ddb34d6ea78d91a
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-08-27 08:43:48 +0700

    Add more documentation, add subclass test

--- lib/Class/Accessor/Array.pm
+++ lib/Class/Accessor/Array.pm
@@ -45,6 +45,7 @@ In F<lib/Your/Class.pm>:
 
  package Your::Class;
  use Class::Accessor::Array {
+     # constructor => 'new',
      accessors => {
          foo => 0,
          bar => 1,
@@ -67,6 +68,25 @@ C<$obj> is now:
 =head1 DESCRIPTION
 
 This module is a builder for array-backed classes.
+
+You can change the constructor name from the default C<new> using the
+C<constructor> parameter.
+
+Currently the built constructor does not accept parameters to set the
+attributes, e.g.:
+
+ my $obj = Your::Class->new(foo=>1, bar=>2); # not supported
+
+You have to set the attributes manually:
+
+ # supported
+ my $obj = Your::Class->new;
+ $obj->foo(1);
+ $obj->bar(2);
+
+If you subclass from another class that uses L<Class::Accessor::Array>, you must
+make sure that you choose an index that has not already been used (unless you
+deliberately want to share storage space with an existing attribute).
 
 Note that if you're looking to reduce memory storage usage, an object based on
 Perl array is not that much-more-space-efficient compared to the hash-based
--- t/01-basic.t
+++ t/01-basic.t
@@ -11,6 +11,7 @@ use Test::More 0.98;
 
 use Local::C1;
 use Local::C2;
+use Local::C3;
 
 my $c1 = Local::C1->new;
 #is_deeply($c1, bless([undef, undef], "Local::C1"));
@@ -24,5 +25,11 @@ my $c2 = Local::C2->spawn;
 $c2->foo(1981);
 $c2->bar(11);
 is_deeply($c2, bless([1981, 11], "Local::C2"));
+
+my $c3 = Local::C3->new;
+$c3->foo(1981);
+$c3->bar(11);
+$c3->baz("a");
+is_deeply($c3, bless([1981, 11, "a"], "Local::C3"));
 
 done_testing;
